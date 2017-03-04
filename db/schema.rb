0d4afaa29500cdb733e1a9a5bcf9d27dff402443
Patrick Mounir <patrickmounir@gmail.com>
2017-03-04 12:07:27 +0200

    Adding comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170304100335) do
+ActiveRecord::Schema.define(version: 20170304100612) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "hotels", force: :cascade do |t|
     t.string   "website"
