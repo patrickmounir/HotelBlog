3f805a34119fe900295a00bd54ccc621b08556d5
Patrick Mounir <patrickmounir@gmail.com>
2017-03-05 00:18:36 +0200

    User being saved

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,14 @@ class UsersController < ApplicationController
 
   end
   def create
-    render plain: params[:user].inspect
+    @user  = User.create(user_params)
+    if @user.save
+      render json: @user
+    else
+      render json: @user.errors.messages
+  end
+end
+  def user_params
+    params.require(:user).permit(:first_name, :last_name,:email,:password)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170304120926) do
+ActiveRecord::Schema.define(version: 20170304221528) do
 
   create_table "assignments", force: :cascade do |t|
     t.integer  "hotel_id"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20170304120926) do
     t.boolean  "verified"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "password"
   end
 
 end
