372a7f7007a34bc478c783a2ca0eebfa1d9cd8b8
Patrick Mounir <patrickmounir@gmail.com>
2017-03-04 12:09:16 +0200

    Services added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170304100612) do
+ActiveRecord::Schema.define(version: 20170304100801) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "text"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 20170304100612) do
   create_table "reviews", force: :cascade do |t|
     t.string   "message"
     t.decimal  "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.string   "name"
+    t.string   "logo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
